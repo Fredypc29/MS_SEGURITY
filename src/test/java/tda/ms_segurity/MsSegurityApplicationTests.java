35e0529b9c630b32fc972ca2a94381fa35476c45
fredypanduro <132675780+Fredypc29@users.noreply.github.com>
2023-12-16 19:34:25 -0500

    Initial commit

---
+++ src/test/java/tda/ms_segurity/MsSegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tda.ms_segurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
