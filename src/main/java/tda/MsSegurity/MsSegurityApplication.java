485c8b425b27c9cb6dc77685f3bae005b06c4601
fredypanduro <132675780+Fredypc29@users.noreply.github.com>
2023-12-17 20:27:11 -0500

    Create MsSegurityApplication.java

---
+++ src/main/java/tda/MsSegurity/MsSegurityApplication.java
@@ -0,0 +1,13 @@
+package tda.MsSegurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSegurityApplication.class, args);
+	}
+
+}
