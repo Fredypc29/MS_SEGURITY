3838549c5d267651d383375323672365e149721e
RoberthAdelmoHoyosZuniga <149548258+RoberthAdelmoHoyosZuniga@users.noreply.github.com>
2023-12-17 19:50:29 -0500

    Update AuthController.java

--- src/main/java/tda/ms_segurity/controller/AuthController.java
+++ src/main/java/tda/ms_segurity/controller/AuthController.java
@@ -1,6 +1,18 @@
 package tda.ms_segurity.controller;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ch.qos.logback.classic.Logger;
+
 public class AuthController {
+private Logger logger = LoggerFactory.getLogger(AuthController.class);
+	@Autowired
+	private IAuthService authService;
+
+	@Autowired
+	private UtilsProperties prop;
+	
 
 }
 
