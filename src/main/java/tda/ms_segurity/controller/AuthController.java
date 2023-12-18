faad05ffbabf09c5a80df7551e2c95bde0f22c53
RoberthAdelmoHoyosZuniga <149548258+RoberthAdelmoHoyosZuniga@users.noreply.github.com>
2023-12-17 19:55:58 -0500

    mmm

--- src/main/java/tda/ms_segurity/controller/AuthController.java
+++ src/main/java/tda/ms_segurity/controller/AuthController.java
@@ -1,9 +1,27 @@
 package tda.ms_segurity.controller;
 
-import org.slf4j.LoggerFactory;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.classic.Logger;
+import TDA.MsSecurity.config.UtilsProperties;
+import TDA.MsSecurity.dto.AuthDTO;
+import TDA.MsSecurity.model.UsuarioModel;
+import TDA.MsSecurity.services.IAuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class AuthController {
 private Logger logger = LoggerFactory.getLogger(AuthController.class);
@@ -12,7 +30,41 @@ private Logger logger = LoggerFactory.getLogger(AuthController.class);
 
 	@Autowired
 	private UtilsProperties prop;
-	
+	@GetMapping("/getAll")
+	public ResponseEntity<?> getAll() {
+		try {
+			List<UsuarioModel> listaAuth = authService.findAll();
+
+			List<AuthDTO> listaDTO = listaAuth.stream().map(x -> {
+				ModelMapper m = new ModelMapper();
+				return m.map(x, AuthDTO.class);
+			}).collect(Collectors.toList());
+
+			return new ResponseEntity<>(listaDTO, HttpStatus.OK);
+			
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(prop.MSG_ERROR_RECUPERAR);
+		}
+	}
+	@GetMapping("/getById")
+	public ResponseEntity<?> getById(int id) {
+		try {
+			ModelMapper m = new ModelMapper();
+			UsuarioModel obj = authService.findById(id);
+			
+			if(obj != null) {
+				AuthDTO dto = m.map(authService.findById(id), AuthDTO.class);
+
+				return new ResponseEntity<>(dto, HttpStatus.OK);
+			}else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(prop.MSG_NO_ENCONTRADO);
+			}
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(prop.MSG_ERROR_RECUPERAR);
+		}
+	}
 
 }
 
