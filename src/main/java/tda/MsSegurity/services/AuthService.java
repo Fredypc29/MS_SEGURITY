3bf9871b1e6b169cf33172a537f0cf6a878b29a0
fredypanduro <132675780+Fredypc29@users.noreply.github.com>
2023-12-18 11:01:10 -0500

    Update AuthService.java

--- src/main/java/tda/MsSegurity/services/AuthService.java
+++ src/main/java/tda/MsSegurity/services/AuthService.java
@@ -1,11 +1,12 @@
 package tda.MsSegurity.services;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
  
-import TDA.MsSecurity.model.UsuarioModel;
+import tda.MsSegurity.model.UsuarioModel;
 import TDA.MsSecurity.repository.IAuthRepository;
 
 public class AuthService implements IAuthService {
