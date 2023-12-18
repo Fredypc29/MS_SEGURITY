b899a692192de8452fb736ced6c0b1cc9a6f34aa
fredypanduro <132675780+Fredypc29@users.noreply.github.com>
2023-12-17 20:27:14 -0500

    Create AuthDTO.java

---
+++ src/main/java/tda/MsSegurity/dto/AuthDTO.java
@@ -0,0 +1,32 @@
+package tda.MsSegurity.dto;
+
+public class AuthDTO {
+    public int idUsuario;
+	public String username;
+	public String password;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String userName) {
+		this.username = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
